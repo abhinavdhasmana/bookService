fff9c17af289e015f0b81807fa5f6381cd518d50
abhinavdhasmana <abhinav.dhasmana@live.com>
2019-02-04 14:31:10 +0530

    added method for get rating from bookId

--- test/util/books/index.test.js
+++ test/util/books/index.test.js
@@ -2,12 +2,19 @@
  * @jest-environment node
  */
 
-const { getBooksWithoutRating } = require('../../../util/books');
+const { getBooksWithoutRating, getRatingFromBookId } = require('../../../util/books');
 
 
 describe('getBooksWithoutRating', () => {
   it('should fetch all the books from the external API', async () => {
     const allBooks = await getBooksWithoutRating();
     expect(Array.isArray(allBooks.books)).toEqual(true);
+  });
+});
+
+describe('getRatingFromBookId', () => {
+  it('should fetch rating of a book with a given id', async () => {
+    const rating = await getRatingFromBookId(10);
+    expect(typeof (rating.rating)).toEqual('number');
   });
 });
--- util/books/index.js
+++ util/books/index.js
@@ -5,4 +5,8 @@ module.exports = {
     const externalUrl = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks';
     return httpGet(externalUrl).then(apiResult => apiResult.data);
   },
+  getRatingFromBookId: (id) => {
+    const externalUrl = `https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${id}`;
+    return httpGet(externalUrl).then(apiResult => apiResult.data);
+  },
 };
